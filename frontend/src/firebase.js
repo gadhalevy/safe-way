f532d1fd088714cb48e5acb814b318b423f613a0
gadhalevy <gad3dh@gmail.com>
2026-05-05 07:35:05 +0300

    move firebase config to env vars

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,19 +1,8 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-import { getAuth } from 'firebase/auth';
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyCGBbLRy19hyKjMBEGgF0SH5Jlv2eVIHlE',
-  authDomain: 'safe-way-504b4.firebaseapp.com',
-  projectId: 'safe-way-504b4',
-  storageBucket: 'safe-way-504b4.firebasestorage.app',
-  messagingSenderId: '366634986777',
-  appId: '1:366634986777:web:89c373076a6da3464aa988',
-  measurementId: 'G-TJT5VJW6RM',
+  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             process.env.REACT_APP_FIREBASE_APP_ID,
 };
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const auth = getAuth(app);
